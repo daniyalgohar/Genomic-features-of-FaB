9d20ce3a457b83e96573f74c2436c72a0c1cb886
Daniyal Gohar <46169300+daniyalgohar@users.noreply.github.com>
2023-12-22 22:13:50 +0200

    Update rpsblast_parallel.sh

--- Sequence analysis/rpsblast_parallel.sh
+++ Sequence analysis/rpsblast_parallel.sh
@@ -7,9 +7,7 @@
 #SBATCH --mem=200000
 #The maximum walltime of the job is 7 days
 #SBATCH -t 170:00:00
-#SBATCH --mail-user=daniyal.gohar@ut.ee
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
+
 module load parallel
 
 ls *.faa | parallel -a - /path/to/directory/gohar/RPS_Blast/ncbi-blast-2.12.0+/bin/rpsblast -query {} -db /path/to/directory/gohar/RPS_Blast/ncbi-blast-2.12.0+/Cog_db/Cog -evalue 0.01 -qcov_hsp_perc 70 -outfmt 6 -out {.}.tsv -num_threads 8
