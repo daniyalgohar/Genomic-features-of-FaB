06ecb052a52b8aab2aa19a22a8af2b9d05f16f23
Daniyal Gohar <46169300+daniyalgohar@users.noreply.github.com>
2023-12-22 22:11:36 +0200

    Update edit_RPS_output_files.sh

--- Sequence analysis/edit_RPS_output_files.sh
+++ Sequence analysis/edit_RPS_output_files.sh
@@ -7,9 +7,7 @@
 #SBATCH --mem=80000
 #The maximum walltime of the job is 7 days
 #SBATCH -t 170:00:00
-#SBATCH --mail-user=daniyal.gohar@ut.ee
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
+
 
 # tag proteins with their origin/sample names
 #Add $filename string to the first column
