39158cce43b6b389296a36a018f58e23c235f025
Daniyal Gohar <46169300+daniyalgohar@users.noreply.github.com>
2023-12-22 22:12:11 +0200

    Update dbcan_CAZyme.sh

--- Sequence analysis/dbcan_CAZyme.sh
+++ Sequence analysis/dbcan_CAZyme.sh
@@ -7,15 +7,7 @@
 #SBATCH --mem=80000
 #The maximum walltime of the job is 7 days
 #SBATCH -t 170:00:00
-#SBATCH --mail-user=daniyal.gohar@ut.ee
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
 
-#CONDA_PATH=$(conda info | grep -i 'base environment' | awk '{print $4}')
-#source $CONDA_PATH/etc/profile.d/conda.sh
-#
-#conda init bash
-#module load python/3.8
 
 module load any/python/3.8.3-conda
 module load parallel
