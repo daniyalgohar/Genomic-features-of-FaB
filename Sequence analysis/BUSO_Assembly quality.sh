7c5290eb58ba53bd82da484950585371b0477f31
Daniyal Gohar <46169300+daniyalgohar@users.noreply.github.com>
2023-12-22 22:07:12 +0200

    Update BUSO_Assembly quality.sh

--- Sequence analysis/BUSO_Assembly quality.sh
+++ Sequence analysis/BUSO_Assembly quality.sh
@@ -6,14 +6,12 @@
 #SBATCH --mem=100GB
 #The maximum walltime of the job is 7 days
 #SBATCH -t 170:00:00
-#SBATCH --mail-user=daniyal.gohar@ut.ee
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
+
 uname -a
 
 
 source activate buscoQC
 
-busco -i /path/to/dir/gohar/BUSCO_QC/sample_data/2757320534.fna -l bacteria_odb10 -o 534qc -m genome
+busco -i /path/to/dir/sample_data/2757320534.fna -l bacteria_odb10 -o 534qc -m genome
 
 
